3cabb00348d68e082223981d4d1ed2fe381ef4a9
HectorSolis <hecsolmay@gmail.com>
2023-01-28 19:16:08 -0600

    agregando rutas a la aplicacion

--- src/app.js
+++ src/app.js
@@ -1,31 +1,32 @@
-const express = require('express');
-const morgan = require('morgan');
-const { createRoles } = require('./libs/initialSetup');
+const express = require("express");
+const morgan = require("morgan");
+const { createRoles } = require("./libs/initialSetup");
 
-const authorization = require('./routes/auth.routes');
-const categories = require('./routes/categories.routes');
-const orders = require('./routes/orders.routes');
-const products = require('./routes/products.routes');
-const sales = require('./routes/sales.routes');
-const users = require('./routes/user.routes');
-
+const authorization = require("./routes/auth.routes");
+const categories = require("./routes/categories.routes");
+const dashboard = require("./routes/dashboard.routes");
+const orders = require("./routes/orders.routes");
+const products = require("./routes/products.routes");
+const sales = require("./routes/sales.routes");
+const users = require("./routes/user.routes");
 
 const app = express();
 createRoles();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    res.send("hola mundo")
-})
+app.get("/", (req, res) => {
+  res.send("hola mundo");
+});
 
-app.use('/auth', authorization)
-app.use('/category', categories)
-app.use('/order', orders)
-app.use('/product', products);
-app.use('/sale', sales)
-app.use('/user', users)
+app.use("/auth", authorization);
+app.use("/category", categories);
+app.use("/order", orders);
+app.use("/product", products);
+app.use("/sale", sales);
+app.use("/user", users);
+app.use("/dashboard", dashboard);
 
 module.exports = app;

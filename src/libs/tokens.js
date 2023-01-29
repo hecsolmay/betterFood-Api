28a58a94486690521a275eb916dc4a0d58aaca0e
HectorSolis <hecsolmay@gmail.com>
2023-01-28 19:10:07 -0600

    implementando jsonwebtokens

--- src/libs/tokens.js
+++ src/libs/tokens.js
@@ -1,21 +1,28 @@
 const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 
-const tokenSign = (user) => {
+const tokenSign = (user,  rol = "user",expires = "24h") => {
+
+
   return jwt.sign(
-    { id: user._id, email: user.email, roles: user.roles },
+    { id: user._id, email: user.email, rol: rol },
     config.SECRET,
     {
-      expiresIn: 86400, // 24 hours
+      expiresIn: expires, // 24 hours
     }
   );
 };
 
 const tokenVerify = (token) => {
+  return jwt.verify(token, config.SECRET);
+};
+
+const tokenDecode = (token) => {
   return jwt.decode(token);
 };
 
 module.exports = {
   tokenSign,
   tokenVerify,
+  tokenDecode,
 };

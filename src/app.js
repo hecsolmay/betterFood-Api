73248f09395ea6190df56d692e07bffb3033cc46
HectorSolis <hecsolmay@gmail.com>
2023-02-15 07:12:44 -0600

    Creando nuevos endpoints

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 const { createRoles } = require("./libs/initialSetup");
 
 const authorization = require("./routes/auth.routes");
 const categories = require("./routes/categories.routes");
 const dashboard = require("./routes/dashboard.routes");
+const ingredents = require("./routes/ingredents.routes");
 const orders = require("./routes/orders.routes");
 const products = require("./routes/products.routes");
 const sales = require("./routes/sales.routes");
 const users = require("./routes/user.routes");
+const waiters = require("./routes/waiter.routes");
 
 const app = express();
 createRoles();
@@ -25,10 +27,12 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authorization);
 app.use("/category", categories);
+app.use("/dashboard", dashboard);
+app.use("/waiter", waiters);
+app.use("/ingredent", ingredents);
 app.use("/order", orders);
 app.use("/product", products);
 app.use("/sale", sales);
 app.use("/user", users);
-app.use("/dashboard", dashboard);
 
 module.exports = app;

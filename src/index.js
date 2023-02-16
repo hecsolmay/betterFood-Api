fb76e7f74ad4cfa0bbd7fe38e0eb70605f94121a
HectorSolis <hecsolmay@gmail.com>
2023-02-16 00:54:43 -0600

    Preparando puerto para produccion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-require('./database');
-const app = require('./app');
+require("./database");
+const app = require("./app");
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

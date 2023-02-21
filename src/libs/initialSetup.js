a01f40e989405df0266c730bb3790fb9c59c2606
HectorSolis <hecsolmay@gmail.com>
2023-02-20 18:43:12 -0600

    Cambiando ejecucion inicial

--- src/libs/initialSetup.js
+++ src/libs/initialSetup.js
@@ -2,17 +2,15 @@ const Role = require("../models/Role");
 
 module.exports.createRoles = async () => {
   try {
-    // const count = await Role.estimatedDocumentCount();
+    const count = await Role.estimatedDocumentCount();
 
-    // if (count > 0) return;
+    if (count > 0) return;
 
-    // const values = await Promise.all([
-    //   new Role({ name: "user" }).save(),
-    //   new Role({ name: "moderator" }).save(),
-    //   new Role({ name: "admin" }).save(),
-    // ]);
-
-    // console.log(values);
+    const values = await Promise.all([
+      new Role({ name: "user" }).save(),
+      new Role({ name: "moderator" }).save(),
+      new Role({ name: "admin" }).save(),
+    ]);
   } catch (error) {
     console.error(error);
   }
